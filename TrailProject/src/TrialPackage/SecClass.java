0a3dc3507fa2acabbb1c2c0686a060e25dd3b80f
AMIWU <ankitadas.b@gmail.com>
2020-10-20 15:23:36 +0530

    first commit

---
+++ TrailProject/src/TrialPackage/SecClass.java
@@ -0,0 +1,9 @@
+package TrialPackage;
+
+public class SecClass {
+
+	public static void main(String[] args) {
+		System.out.println("my second class output");
+	}
+
+}
---
+++ TrailProject/src/TrialPackage/TrailClass.java
@@ -0,0 +1,9 @@
+package TrialPackage;
+
+public class TrailClass {
+
+	public static void main(String[] args) {
+		System.out.println("from eclipse to github");
+	}
+
+}
